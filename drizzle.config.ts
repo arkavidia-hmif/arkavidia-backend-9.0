6d6ca67b46ec059bc73355b6f98773259be802be
Fawwaz Abrial Saffa <18221067@std.stei.itb.ac.id>
2024-11-30 17:23:31 +0700

    chore: initialize framework

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+	dialect: 'postgresql',
+	schema: './src/db/schema',
+	out: './drizzle',
+	dbCredentials: {
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		url: process.env.DATABASE_URL!,
+	},
+});
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { swaggerUI } from '@hono/swagger-ui';
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { serve } from 'bun';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { env } from './configs/env.config';
+import { apiRouter } from './controllers/api.controller';
+import { apiReference } from '@scalar/hono-api-reference'
+
+
+const app = new OpenAPIHono({
+	defaultHook: (result, c) => {
+		if (!result.success) {
+			return c.json({ errors: result.error.flatten() }, 400);
+		}
+	},
+});
+
+app.use(
+	'/api/*',
+	cors({
+		credentials: true,
+		origin: env.ALLOWED_ORIGINS,
+	}),
+);
+
+app.get('/', (c) => c.json({ message: 'Server runs successfully' }));
+
+app.route('/api', apiRouter);
+
+app.doc('/openapi.json', {
+	openapi: '3.1.0',
+	info: {
+		version: '1.0',
+		title: 'Competition Site API',
+	},
+	tags: [
+		{ name: 'hello', description: 'Hello API' },
+		{ name: 'todo', description: 'Todo API' },
+	],
+});
+
+app.get(
+  '/docs',
+  apiReference({
+    theme: 'purple',
+    spec: {
+      url: '/openapi.json',
+    },
+  }),
+)
+
+console.log(`Server is running on port ${env.PORT}`);
+
+serve({
+	fetch: app.fetch,
+	port: env.PORT,
+});
