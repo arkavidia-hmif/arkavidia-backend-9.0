e483084bfc5f1da1be589a65476b75b3d08123d8
jonnjonnjo <zhenagajo234@gmail.com>
2024-12-09 10:19:35 +0700

    format : reformat using biome

--- src/controllers/team.controller.ts
+++ src/controllers/team.controller.ts
@@ -1,29 +1,33 @@
-import { db } from '../db/drizzle';
-import { insertUserToTeam } from '../repositories/team-member.repository';
-import { createTeam } from '../repositories/team.repository';
-import { postCreateTeamRoute } from '../routes/team.route';
-import { createAuthRouter } from '~/utils/router-factory';
+import { db } from "../db/drizzle";
+import { insertUserToTeam } from "../repositories/team-member.repository";
+import { createTeam } from "../repositories/team.repository";
+import { postCreateTeamRoute } from "../routes/team.route";
+import { createAuthRouter } from "~/utils/router-factory";
 
 export const teamProtectedRouter = createAuthRouter();
 
-teamProtectedRouter.openapi(postCreateTeamRoute, async (c)=>{
-  try{
-    const {competitionId, name} = await c.req.json(); 
-    const team = await createTeam(db,competitionId,name);
+teamProtectedRouter.openapi(postCreateTeamRoute, async (c) => {
+  try {
+    const { competitionId, name } = await c.req.json();
+    const team = await createTeam(db, competitionId, name);
     const userId = c.var.user.id;
-    await insertUserToTeam(db,team.id,userId);
-
-    return c.json(team,200);
-  }catch(error){
-    
-    if( error instanceof Error){
-      return c.json({
-        error:error.message,
-      },500);
+    await insertUserToTeam(db, team.id, userId);
+    return c.json(team, 200);
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json(
+        {
+          error: error.message,
+        },
+        500,
+      );
     }
 
-    return c.json({
-      error:"Unexpected error occured"
-    },500);
+    return c.json(
+      {
+        error: "Unexpected error occured",
+      },
+      500,
+    );
   }
-})
+});
--- src/repositories/team.repository.ts
+++ src/repositories/team.repository.ts
@@ -1,63 +1,73 @@
-import { eq } from 'drizzle-orm';
-import type { Database } from '~/db/drizzle';
-import { team } from '~/db/schema';
-import type { TeamMemberRelationOption } from './team-member.repository';
-import { getCompetitionById, getCompetitionParticipantNumber } from './competition.repository';
+import { eq } from "drizzle-orm";
+import type { Database } from "~/db/drizzle";
+import { team } from "~/db/schema";
+import type { TeamMemberRelationOption } from "./team-member.repository";
+import {
+  getCompetitionById,
+  getCompetitionParticipantNumber,
+} from "./competition.repository";
 
 interface TeamRelationOption {
-	teamMember?: TeamMemberRelationOption | boolean;
-	competition?: boolean;
-	paymentProof?: boolean;
+  teamMember?: TeamMemberRelationOption | boolean;
+  competition?: boolean;
+  paymentProof?: boolean;
 }
 
 export const getTeamById = async (
-	db: Database,
-	teamId: string,
-	options?: TeamRelationOption,
+  db: Database,
+  teamId: string,
+  options?: TeamRelationOption,
 ) => {
-	return await db.query.team.findFirst({
-		where: eq(team.id, teamId),
-		with: {
-			teamMembers:
-				typeof options?.teamMember === 'boolean'
-					? options?.teamMember
-						? true
-						: undefined
-					: {
-							with: {
-								user: options?.teamMember?.user ? true : undefined,
-								nisn: options?.teamMember?.nisn ? true : undefined,
-								kartu: options?.teamMember?.kartu ? true : undefined,
-								poster: options?.teamMember?.poster ? true : undefined,
-								twibbon: options?.teamMember?.twibbon ? true : undefined,
-							},
-						},
-			competition: options?.competition ? true : undefined,
-			paymentProof: options?.paymentProof ? true : undefined,
-		},
-	});
+  return await db.query.team.findFirst({
+    where: eq(team.id, teamId),
+    with: {
+      teamMembers:
+        typeof options?.teamMember === "boolean"
+          ? options?.teamMember
+            ? true
+            : undefined
+          : {
+              with: {
+                user: options?.teamMember?.user ? true : undefined,
+                nisn: options?.teamMember?.nisn ? true : undefined,
+                kartu: options?.teamMember?.kartu ? true : undefined,
+                poster: options?.teamMember?.poster ? true : undefined,
+                twibbon: options?.teamMember?.twibbon ? true : undefined,
+              },
+            },
+      competition: options?.competition ? true : undefined,
+      paymentProof: options?.paymentProof ? true : undefined,
+    },
+  });
 };
 
-export const createTeam = async(
+export const createTeam = async (
   db: Database,
   competitionId: string,
   name: string,
-) =>{
-  return await db.transaction(async(tx)=>{
-   // check whether we still could insert another team
-    const {participantCount} = await getCompetitionParticipantNumber(db,competitionId);
-    const {maxParticipants} = await getCompetitionById(db,competitionId);
-    
-    if(maxParticipants <= participantCount ){
+) => {
+  return await db.transaction(async (tx) => {
+    const { participantCount } = await getCompetitionParticipantNumber(
+      db,
+      competitionId,
+    );
+    const { maxParticipants } = await getCompetitionById(db, competitionId);
+
+    if (maxParticipants <= participantCount) {
       // return an error ?
-      throw new Error("Maximum number of participants reached for this competition.");
+      throw new Error(
+        "Maximum number of participants reached for this competition.",
+      );
     }
 
-    const [insertedTeam] = await tx.insert(team).values({
-      competitionId,
-      name
-    }).returning();
+    const [insertedTeam] = await tx
+      .insert(team)
+      .values({
+        competitionId,
+        name,
+      })
+      .returning();
 
-    return insertedTeam; 
+    return insertedTeam;
   });
-}
+};
