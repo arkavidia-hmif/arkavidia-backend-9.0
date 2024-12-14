2be9fdea2db2fa4c6e564832e94332b8c175f628
albertchriss <19623210@std.stei.itb.ac.id>
2024-12-14 09:28:39 +0700

    feat: admin get and post announcement

--- src/repositories/competition.repository.ts
+++ src/repositories/competition.repository.ts
@@ -1,6 +1,9 @@
 import { eq } from 'drizzle-orm';
 import type { Database } from '../db/drizzle';
-import { competition, team } from '../db/schema';
+import { competition, competitionAnnouncement, team } from '../db/schema';
+import type { PostCompAnnouncementBodySchema } from '~/types/competition.type';
+import type { z } from 'zod';
+import { first } from '~/db/helper';
 
 export const getCompetitionParticipantNumber = async (
 	db: Database,
@@ -21,4 +24,42 @@ export const getCompetitionById = async (
 	});
 
 	return { maxParticipants: result?.maxParticipants };
+};
+
+export const getCompetition = async (db: Database, competitionId: string) => {
+	const result = await db.query.competition.findFirst({
+		where: eq(competition.id, competitionId),
+	});
+	return result;
+};
+
+export const getAnnouncementsByCompetitionId = async (
+	db: Database,
+	competitionId: string,
+) => {
+	const result = await db.query.competitionAnnouncement.findMany({
+		where: eq(competitionAnnouncement.competitionId, competitionId),
+	});
+	return result;
+};
+
+export const postAnnouncement = async (
+	db: Database,
+	competitionId: string,
+	authorId: string,
+	body: z.infer<typeof PostCompAnnouncementBodySchema>,
+) => {
+	return await db.transaction(async (trx) => {
+		const announcement = await trx
+			.insert(competitionAnnouncement)
+			.values({
+				competitionId: competitionId,
+				authorId: authorId,
+				title: body.title,
+				description: body.description,
+			})
+			.returning()
+			.then(first);
+		return announcement;
+	});
 };
