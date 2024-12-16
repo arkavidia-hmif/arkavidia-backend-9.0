f4ae1e18170975b407ba9e5e338d1df3f4083010
Valentino Triadi <13522164@std.stei.itb.ac.id>
2024-12-16 14:40:35 +0700

    fix: prettier & eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    ],
+  ignorePatterns: ['.eslintrc.js', 'node_modules/', 'drizzle/'],
+  overrides: [
+    {
+      files: ['.eslintrc.{js,cjs}'],
+      env: {
+        node: true,
+      },
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
+  },
+};
