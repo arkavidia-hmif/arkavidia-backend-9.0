3bd4caa88176f6580346ab5a2842817f2d0f7703
ValentinoTriadi <13522164@std.stei.itb.ac.id>
2025-01-20 10:47:57 +0700

    fix: add stage to team

--- src/db/schema/team.schema.ts
+++ src/db/schema/team.schema.ts
@@ -2,7 +2,11 @@ import { relations } from 'drizzle-orm';
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 import { createId, getNow } from '../../utils/drizzle-schema-util';
-import { competition, competitionSubmission } from './competition.schema';
+import {
+  competition,
+  competitionSubmission,
+  stageEnum,
+} from './competition.schema';
 import { teamMember } from './team-member.schema';
 import { teamDocument } from './verification.schema';
 
@@ -12,6 +16,7 @@ export const team = pgTable('team', {
     .notNull()
     .references(() => competition.id, { onDelete: 'cascade' }), // Add reference to competition
   name: text('team_name').notNull(),
+  stage: stageEnum('stage').notNull().default('pre-eliminary'),
   joinCode: text('team_code').notNull().$defaultFn(createId).unique(), // Add unique constraint
   createdAt: timestamp('created_at').defaultNow(),
   updatedAt: timestamp('updated_at').$onUpdate(getNow),
