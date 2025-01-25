60dd7508cdc78777b236343a51233e1e9bb3937f
fawwazabrials <fawwazabrial.fas@gmail.com>
2025-01-26 00:42:23 +0700

    fix: migration enum issues (again...)

--- drizzle/0005_needy_spiral.sql
+++ drizzle/0005_needy_spiral.sql
@@ -1,8 +1,4 @@
 ALTER TABLE "competition_submission_requirement" ADD COLUMN "order" integer DEFAULT -1 NOT NULL;--> statement-breakpoint
--- ALTER TABLE "public"."user_identity" ALTER COLUMN "role" SET DATA TYPE text;--> statement-breakpoint
--- DROP TYPE "public"."user_identity_role_enum";--> statement-breakpoint
--- CREATE TYPE "public"."user_identity_role_enum" AS ENUM(
-ALTER TABLE "public"."user_identity" ALTER COLUMN "role" SET DATA TYPE "public"."user_identity_role_enum" USING "role"::"public"."user_identity_role_enum";--> statement-breakpoint
 ALTER TYPE user_identity_role_enum ADD VALUE 'superadmin';--> statement-breakpoint
 ALTER TYPE user_identity_role_enum ADD VALUE 'admin_competition';--> statement-breakpoint
 ALTER TYPE user_identity_role_enum ADD VALUE 'admin_competition_cp';--> statement-breakpoint
@@ -16,4 +12,3 @@ ALTER TYPE user_identity_role_enum ADD VALUE 'admin_event_academya_softeng';--> 
 ALTER TYPE user_identity_role_enum ADD VALUE 'admin_event_academya_datsci';--> statement-breakpoint
 ALTER TYPE user_identity_role_enum ADD VALUE 'admin_event_academya_uiux';--> statement-breakpoint
 ALTER TYPE user_identity_role_enum ADD VALUE 'admin_event_academya_pm';--> statement-breakpoint
-ALTER TYPE user_identity_role_enum DROP VALUE 'admin';--> statement-breakpoint
