70b519e0cb5365b4ee7178b32bd43919d65fd9e7
fawwazabrials <fawwazabrial.fas@gmail.com>
2025-01-26 00:44:54 +0700

    feat: register admin routes

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -11,6 +11,7 @@ import { mediaRouter } from './media.controller';
 import { teamMemberProtectedRouter } from './team-member.controller';
 import { teamProtectedRouter } from './team.controller';
 import { userProtectedRouter } from './user.controller';
+import { adminCompetitionProtectedRouter } from './admin-competition.controller';
 
 const unprotectedApiRouter = new OpenAPIHono();
 unprotectedApiRouter.route('/', healthRouter);
@@ -26,6 +27,7 @@ protectedApiRouter.route('/', teamProtectedRouter);
 protectedApiRouter.route('/', userProtectedRouter);
 protectedApiRouter.route('/', competitionProtectedRouter);
 protectedApiRouter.route('/', analyticsProtectedRouter);
+protectedApiRouter.route('/', adminCompetitionProtectedRouter)
 
 export const apiRouter = new OpenAPIHono();
 apiRouter.route('/', unprotectedApiRouter);
