258b45306f6d2f09da8c29e392fcb8fbf6f32e96
fawwazabrials <fawwazabrial.fas@gmail.com>
2025-02-04 09:02:28 +0700

    feat: add groupchat to email

--- src/utils/competition-utils.ts
+++ src/utils/competition-utils.ts
@@ -8,3 +8,18 @@ export const expandCompetitionTitle = (title: string) => {
       return title;
   }
 };
+
+export const getCompetitionGroupChat = (title: string) => {
+  switch (title) {
+    case 'Arkalogica':
+      return 'https://chat.whatsapp.com/DQQRLeR6bJL5GLngewQgjI';
+    case 'CP':
+      return 'https://discord.gg/KAvfwuTbpg';
+    case 'CTF':
+      return 'https://s.hmif.dev/DiscordCTFArkavidia';
+    case 'Datavidia':
+      return 'https://s.hmif.dev/GrupDatavidia9';
+    default:
+      return undefined;
+  }
+};
