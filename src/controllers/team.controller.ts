ba2f9c71e24281d868f3649049932f28e9631a07
Fawwaz Abrial Saffa <18221067@std.stei.itb.ac.id>
2025-02-08 19:15:15 +0700

    fix: max length on creating team

--- src/controllers/team.controller.ts
+++ src/controllers/team.controller.ts
@@ -60,6 +60,12 @@ teamProtectedRouter.openapi(postCreateTeamRoute, async (c) => {
     const { competitionId, name } = await c.req.json();
     const userId = c.var.user.id;
 
+    if (name.length > 50)
+      return c.json(
+        { error: 'Length of team name must be less than 50 characters!' },
+        400,
+      );
+
     const user = await getUser(db, userId);
     const competition = await getCompetitionById(db, competitionId);
     if (competition?.title === 'Arkalogica' && user?.education !== 'sma')
