06ac70d29f55f69651b23dfec03ecea8bd00a342
Novita Setiawan <99242334+NovitaSetiawati067@users.noreply.github.com>
2025-02-18 00:02:01 +0700

    feat: put event team verification

--- src/routes/admin-event.route.ts
+++ src/routes/admin-event.route.ts
@@ -1,6 +1,6 @@
 import { createRoute } from '@hono/zod-openapi';
 import { roleMiddleware } from '~/middlewares/role-access.middleware';
-import { PutTeamSubmissionVerdictSchema } from '~/types/admin-competition.type';
+import { PutTeamSubmissionVerdictSchema, PutTeamVerificationBodySchema } from '~/types/admin-competition.type';
 import {
   AdminAllEventTeamQuerySchema,
   EventAndTeamIdAndSubmissionIdParam,
@@ -10,6 +10,7 @@ import {
 } from '~/types/admin-event.type';
 import {
   EventTeamAndEventIdParam,
+  EventTeamSchema,
   PutEventTeamStatusBodySchema,
 } from '~/types/event-team.type';
 import { EventIdParam, ListEventSchema } from '~/types/event.type';
@@ -120,16 +121,25 @@ export const putAdminEventTeamVerificationRoute = createRoute({
   method: 'put',
   middleware: [roleMiddleware('admin_event')] as const,
   path: '/admin/event/{eventId}/team/{teamId}/submission',
-  request: {},
+  request: {
+    params: EventAndTeamIdParam,
+    body: {
+      content: {
+        'application/json': {
+          schema: PutTeamVerificationBodySchema,
+        },
+      },
+    },
+  },
   responses: {
-    // 200: {
-    //   description: "Succesfully updated team's verification status.",
-    //   content: {
-    //     'application/json': {
-    //       schema: TeamSchema,
-    //     },
-    //   },
-    // },
+    200: {
+      description: "Succesfully updated event team's verification status.",
+      content: {
+        'application/json': {
+          schema: EventTeamSchema,
+        },
+      },
+    },
     400: createErrorResponse('UNION', 'Bad request error'),
     403: createErrorResponse('GENERIC', 'Not authorized for access'),
     500: createErrorResponse('GENERIC', 'Internal server error'),
